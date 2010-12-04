743a7c2e332f1da3cbf2a55f0e7d52f00bdc11c8
simonetripodi <simone.tripodi@gmail.com>
2010-12-04 01:45:34 +0100

    first checkin of classvisitor tests

---
+++ src/test/java/org/nnsoft/commons/meiyo/classvisitor/AnnotatedBean.java
@@ -0,0 +1,16 @@
+package org.nnsoft.commons.meiyo.classvisitor;
+
+@ClassAnnotation
+final class AnnotatedBean {
+
+    @ConstructorAnnotation
+    public AnnotatedBean() {
+        
+    }
+
+    @MethodAnnotation
+    public void doNothing() {
+        
+    }
+
+}
---
+++ src/test/java/org/nnsoft/commons/meiyo/classvisitor/ClassVisitorTestCase.java
@@ -0,0 +1,71 @@
+/*
+ *    Copyright 2010 The Meiyo Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.nnsoft.commons.meiyo.classvisitor;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * FILL ME.
+ */
+public final class ClassVisitorTestCase {
+
+    private boolean foundClassAnnotation = false;
+
+    private boolean foundConstructorAnnotation = false;
+
+    private boolean foundMethodAnnotation = false;
+
+    @BeforeClass
+    public void setUp() {
+        ClassVisitor classVisitor = new ClassVisitor();
+        classVisitor.handleClass().annotatedWith(ClassAnnotation.class).withHandler(new AnnotationHandler<Class, ClassAnnotation>() {
+            public void handle(Class annnotatedElement, ClassAnnotation annotation) {
+                foundClassAnnotation = true;
+            }
+        });
+        classVisitor.handleConstructor().annotatedWith(ConstructorAnnotation.class).withHandler(new AnnotationHandler<Constructor, ConstructorAnnotation>() {
+            public void handle(Constructor annnotatedElement, ConstructorAnnotation annotation) {
+                foundConstructorAnnotation = true;
+            }
+        });
+        classVisitor.handleMethod().annotatedWith(MethodAnnotation.class).withHandler(new AnnotationHandler<Method, MethodAnnotation>() {
+            public void handle(Method annnotatedElement, MethodAnnotation annotation) {
+                foundMethodAnnotation = true;
+            }
+        });
+        classVisitor.visit(AnnotatedBean.class);
+    }
+
+    @Test
+    public void verifyClassAnnotationsFound() {
+        assert this.foundClassAnnotation;
+    }
+
+    @Test
+    public void verifyConstructorAnnotationsFound() {
+        assert this.foundConstructorAnnotation;
+    }
+
+    @Test
+    public void verifyMethodAnnotationsFound() {
+        assert this.foundMethodAnnotation;
+    }
+
+}
